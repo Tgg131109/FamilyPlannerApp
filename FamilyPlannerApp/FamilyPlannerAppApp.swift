4ab6a7966dd285f6b3f9b8f8c3c55acdbdcb7a3d
Tgg131109 <tobyg7687@gmail.com>
2025-08-04 09:10:39 -0400

    Initial commit - created iOS app

---
+++ FamilyPlannerApp/FamilyPlannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyPlannerAppApp.swift
+//  FamilyPlannerApp
+//
+//  Created by Toby Gamble on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FamilyPlannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FamilyPlannerAppTests/FamilyPlannerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyPlannerAppTests.swift
+//  FamilyPlannerAppTests
+//
+//  Created by Toby Gamble on 8/4/25.
+//
+
+import Testing
+@testable import FamilyPlannerApp
+
+struct FamilyPlannerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
